6e6b22ebf1c201fd39220a7c757817e9150e542b
Steven Enamakel <enamakel@vezures.com>
2022-02-07 22:10:10 +0000

    added parser for quicksart

--- push-aws-ecr.sh
+++ push-aws-ecr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-aws ecr-public get-login-password --region us-east-1 --profile scallop | docker login --username AWS --password-stdin public.ecr.aws/g2z2e8i7
 docker buildx build --platform linux/amd64 -t mahatrade/hummingbot -f Dockerfile .
+aws ecr-public get-login-password --region us-east-1 --profile scallop | docker login --username AWS --password-stdin public.ecr.aws/g2z2e8i7
 docker tag mahatrade/hummingbot:latest public.ecr.aws/g2z2e8i7/mahatrade/hummingbot:latest
 docker push public.ecr.aws/g2z2e8i7/mahatrade/hummingbot:latest
 
